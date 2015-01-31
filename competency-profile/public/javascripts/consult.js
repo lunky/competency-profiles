28d0562313bb9fea54a3f6031525c2f4009f46f9
Aaron Levine <aaronlevine@gmail.com>
2015-01-30 23:41:34 -0600

    Style Updates
    Some updates to the stylus definitions
    Removed extra style definitions around supporting / counter examples
    Added hover effect to the description box
    Added ng-click function to the description box and check mark on / off
    switch to the objective met div
    Added updates to the controller to enable isMet / !isMet on description
    clcik
    Updated the jade templates to:
    - move the buttons to the top
    - allow the example boxes to grow based on their contents
    - instructions for clicking the description box
    - check mark

--- competency-profile/public/javascripts/consult.js
+++ competency-profile/public/javascripts/consult.js
@@ -6,25 +6,28 @@
         'objectivesService', function (objectivesService) {
             var vm = this;
             vm.objectives = [];
-            vm.currIndex = 0;
-            vm.next = function () {
-                if (vm.currIndex < vm.objectives.length-1) {
-                    vm.currIndex += 1;
-                }
-                vm.curr = vm.objectives[vm.currIndex];
-            }
-            vm.prev = function () {
-                if (vm.currIndex > 0) {
-                    vm.currIndex -= 1;
-                }
-                vm.curr = vm.objectives[vm.currIndex];
-            }
-            vm.initialize = function () {
-                objectivesService.getObjectives().then(function(data) {
-                    vm.objectives = data.data;
-                    vm.curr = vm.objectives[vm.currIndex];
-                });
-            }            
+			vm.currIndex = 0;
+	        vm.next = function() {
+		        if (vm.currIndex < vm.objectives.length - 1) {
+			        vm.currIndex += 1;
+		        }
+		        vm.curr = vm.objectives[vm.currIndex];
+	        };
+	        vm.prev = function() {
+		        if (vm.currIndex > 0) {
+			        vm.currIndex -= 1;
+		        }
+		        vm.curr = vm.objectives[vm.currIndex];
+	        };
+	        vm.initialize = function() {
+		        objectivesService.getObjectives().then(function(data) {
+			        vm.objectives = data.data;
+			        vm.curr = vm.objectives[vm.currIndex];
+		        });
+	        };
+	        vm.meetObjective = function() {
+		        vm.curr.isMet = !vm.curr.isMet;
+	        };
             vm.initialize();
         }
     ]);
