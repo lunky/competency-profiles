f29314e770a424bd1357bb17dec02d0320c0f95f
Quinn Wilson <git@midword.org>
2015-02-05 23:14:11 -0600

    wire up mongo

--- competency-profile/app.js
+++ competency-profile/app.js
@@ -25,15 +25,24 @@ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/objective');
+// Make our db accessible to our router
+app.use(function (req, res, next) {
+	req.db = db;
+	next();
+});
+
 app.use('/', routes);
 app.use('/objectives', objectives);
 app.use('/members', members);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -41,23 +50,23 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 
