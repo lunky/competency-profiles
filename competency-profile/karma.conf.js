5a6ece8cdc169161dd6f725c354dea5e92ef795d
Quinn Wilson <git@midword.org>
2015-02-16 18:14:11 -0600

    fix tests - build dev branch

--- competency-profile/karma.conf.js
+++ competency-profile/karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function(config) {
             'bower_components/angular-mocks/angular-mocks.js',
             'bower_components/angular-route/angular-route.js',
             'bower_components/angular-animate/angular-animate.js',
-            'public/javascripts/*.js',
+            'public/app/**/*.js',
             'test/**/*.js'
     ],
 
