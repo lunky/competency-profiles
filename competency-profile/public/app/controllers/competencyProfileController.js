3ea588303be09cee1afc12bed491c1bf9f095298
Quinn Wilson <git@midword.org>
2015-02-22 15:58:59 -0600

    wire up stats

--- competency-profile/public/app/controllers/competencyProfileController.js
+++ competency-profile/public/app/controllers/competencyProfileController.js
@@ -28,19 +28,8 @@
 		function initialize() {
 			objectivesService.getObjectives().then(function (data) {
 				var objectives = data.data;
-				vm.objectives = objectives;
-				
-				//TODO set real scores here
-				vm.score = {
-					base: 60,
-					intermediate: 30,
-					senior: 5,
-					communication: 70,
-					leadership: 55,
-					interpersonal: 70,
-					conflict: 40,
-					citizenship: 50
-				};
+				vm.objectives = objectives;				
+				vm.score = data.summary;
 			});
 		}
 		
@@ -48,17 +37,8 @@
 			// TODO : filter objectives that have something changed?
 			var objectives = vm.objectives;
 			objectivesService.save(objectives).then(function (data) {
-				//TODO reset real scores here
-				vm.score = {
-					base: vm.score.base + 1,
-					intermediate: vm.score.intermediate + 1,
-					senior: vm.score.senior + 1,
-					communication: vm.score.communication + 1,
-					leadership: vm.score.leadership + 1,
-					interpersonal: vm.score.interpersonal + 1,
-					conflict: vm.score.conflict + 1,
-					citizenship: vm.score.citizenship + 1
-				};
+				var objectives = data.data;
+				vm.score = data.summary;
 			});
 		}
 		
--- competency-profile/routes/objectives.js
+++ competency-profile/routes/objectives.js
@@ -1,83 +1,161 @@
-﻿var express = require('express');
+﻿var express	 = require('express');
 var router = express.Router();
 var Q = require('q');
 var isAuthenticated = require('../config/auth');
 
 /* GET home page. */
 router.get('/', isAuthenticated, function (req, res) {
-	res.render('objectives');
+  res.render('objectives');
 });
 
 router.post('/save', isAuthenticated, function(req, res) {
-	var userid = req.user;
-	var db = req.db;
-	var selfEval = {'userid': userid.username, 'objectivesMet': req.body.objectives};
-	var collection = db.get('objectivesMet');
+  var userid = req.user;
+  var db = req.db;
+  var selfEval = {'userid': userid.username, 'objectivesMet': req.body.objectives};
+  var collection = db.get('objectivesMet');
 
-	collection.findAndModify(
-		{
-			query: {
-				'userid': userid.username
-			},
-			update: selfEval
-		},
-		{
-			'upsert': true
-		},
-		function(err, docs) {
-			if (err) {
-				res.send(err);
-			}
-			console.log('docs: ' + docs);
-			res.send({'score': 'half wit consultant'});
-		}
-	);
+  collection.findAndModify(
+    {
+      query: {
+        'userid': userid.username
+      },
+      update: selfEval
+    },
+    {
+      'upsert': true
+    },
+    function(err, docs) {
+      if (err) {
+        res.send(err);
+      }
+      ObjectivesAndObjectivesMet(req, res, function(metDocument){
+		    res.send({'data': metDocument, 'summary' : GetStats(metDocument)});
+			});
+   }
+  );
 });
 
-router.get('/list', isAuthenticated, function(req, res) {
-
-	var deferred = Q.defer();
-	var collection = req.db.get('objective');
-	var met = req.db.get('objectivesMet');
-	var metdoc;
-	var objectivesdoc;
-	// there are two documents, the objectives master and the list of objectivesMet which is a per user document
-	// we have to merge them but the methods are async so we use promises to collect them when we're done
-	met.findOne({'userid': req.user.username}, function(err, doc) {
-		if (err) {
-			res.send(err);
-		}
-		metdoc = doc;
-		deferred.resolve(doc);
-	});
-
-	var deferred2 = Q.defer();
-	collection.find({}, function(err, doc) {
-		if (err) {
-			res.send(err);
-		}
-		objectivesdoc = doc;
-		deferred2.resolve(doc);
-	});
-
-	Q.all([deferred.promise, deferred2.promise])
-		.then(function() {
-			// merge
-			if (metdoc) {
-				objectivesdoc.forEach(function(el) {
-						if (metdoc.objectivesMet.some(function(metObjective) {
-							return metObjective.objectiveId === el.objectiveId;
-						})) {
-							el.isMet = true;
-						} else {
-							el.isMet = false;
-						}
-					}
-				);
+function GetStats(objectives){
+	var doc = {};
+	var stats = {base:0, intermediate:0, senior:0, baseTotal:0, intermediateTotal:0, seniorTotal:0, 
+		"Communication":0, "Leadership":0, "Interpersonal":0, "Conflict":0, "Citizenship":0,
+		"CommunicationTotal":0, "LeadershipTotal":0, "InterpersonalTotal":0, "ConflictTotal":0, "CitizenshipTotal":0};
+	doc = objectives.reduce(function(prev, curr, idx, arr){
+			if(curr.gate_level==""){
+				prev.baseTotal += curr.score;
+				if(curr.isMet){
+					prev.base += curr.score;
+				}
 			}
-			res.send({'data': objectivesdoc});
-		}
-	);
+			if(curr.gate_level=="intermediate"){
+				prev.intermediateTotal += curr.score;
+				if(curr.isMet){
+					prev.intermediate += curr.score;
+				}
+			}
+			if(curr.gate_level=="senior"){
+				prev.seniorTotal += curr.score;
+				if(curr.isMet){
+					prev.senior += curr.score;
+				}
+			}
+			["Communication", "Leadership", "Interpersonal", "Conflict", "Citizenship"].forEach(function(competency){
+				if(curr[competency] == "Y"){
+					prev[competency + "Total"] += curr.competency_weighting;
+					if(curr.isMet){
+						prev[competency] += curr.competency_weighting;
+					}
+				} 
+			});
+			return prev;
+	}, stats);
+	var summary = {
+					base: Math.round(doc.base/doc.baseTotal*100),
+					intermediate: Math.round(doc.intermediate/doc.intermediateTotal*100),
+					senior: Math.round(doc.senior/doc.seniorTotal*100),
+					communication: Math.round(doc.Communication/doc.CommunicationTotal*100),
+					leadership: Math.round(doc.Leadership/doc.LeadershipTotal*100),
+					interpersonal: Math.round(doc.Interpersonal/doc.InterpersonalTotal*100),
+					conflict: Math.round(doc.Conflict/doc.ConflictTotal*100),
+					citizenship: Math.round(doc.Citizenship/doc.CitizenshipTotal*100)
+				};
+	
+	return summary;
+}
+
+      //Competency Level Percentage Calculation
+      // 1) get the sum of all objectives scores grouped by level 
+      // 2) get the sum of all selected objectives scores grouped by level 
+      // 3) divide
+      
+      //Competency Group Percentage Calculation
+      // 1) get the count of all objectives grouped by competency
+      // 2) get the count of all selected objectives grouped by competency 
+      // 3) divide
+      
+      //Consultant Competency Level Calculation
+      // 1) If 
+      //                      the sum of all selected non-gate objectives is greater than intermediate score
+      //                      &
+      //                      the sum of all selected gated objectives is greater than base intermediate score
+      //                      then consultant is intermediate
+      
+      // 2) If 
+      //                      consultant meets intermediate requirements
+      //                      &
+      //                      the sum of all selected non-gate objectives is greater than senior score
+      //                      &
+      //                      the sum of all selected gated objectives is greater than base senior score
+      //                      then consultant is senior
+
+	function ObjectivesAndObjectivesMet(req, res, done){
+  var deferred = Q.defer();
+  var collection = req.db.get('objective');
+  var met = req.db.get('objectivesMet');
+  var metdoc;
+  var objectivesdoc;
+  // there are two documents, the objectives master and the list of objectivesMet which is a per user document
+  // we have to merge them but the methods are async so we use promises to collect them when we're done
+  met.findOne({'userid': req.user.username}, function(err, doc) {
+    if (err) {
+      res.send(err);
+    }
+    metdoc = doc;
+    deferred.resolve(doc);
+  });
+
+  var deferred2 = Q.defer();
+  collection.find({}, function(err, doc) {
+    if (err) {
+      res.send(err);
+    }
+    objectivesdoc = doc;
+    deferred2.resolve(doc);
+  });
+
+  Q.all([deferred.promise, deferred2.promise])
+    .then(function() {
+      // merge
+      if (metdoc) {
+        objectivesdoc.forEach(function(el) {
+            if (metdoc.objectivesMet.some(function(metObjective) {
+              return metObjective.objectiveId === el.objectiveId;
+            })) {
+              el.isMet = true;
+            } else {
+              el.isMet = false;
+            }
+          }
+        );
+      }
+      done(objectivesdoc);
+    }
+  );
+};
+router.get('/list', isAuthenticated, function(req, res) {
+	ObjectivesAndObjectivesMet(req, res, function(metDocument){
+		    res.send({'data': metDocument, 'summary' : GetStats(metDocument)});
+  });
 });
 
 module.exports = router;
