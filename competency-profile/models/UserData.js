a4bd51ee5151af4ab4c4074079933008c5a3f9c4
ahhchuu <jkhiebert@gmail.com>
2015-02-28 15:47:00 -0600

    Added new displayName field

--- competency-profile/models/UserData.js
+++ competency-profile/models/UserData.js
@@ -3,7 +3,8 @@ var passportLocal = require('passport-local');
 
 var UserSchema = new mongoose.Schema({
 	username: String,
-	password: String
+	password: String,
+	displayName: String
 });
 
 module.exports = mongoose.model('UserData', UserSchema);
