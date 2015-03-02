dca9f054ccb99d15ff1d3e477dad8e0f9a6fde4f
Brian McKay <BKMWpg+git@gmail.com>
2015-03-02 00:49:44 -0600

    more LINT clean up

--- competency-profile/public/app/app.js
+++ competency-profile/public/app/app.js
@@ -4,84 +4,86 @@
 	angular.module('consultingCommon', []);
 	angular.module('consultingServices', ['consultingCommon']);
 	angular.module('consultingControllers', ['consultingCommon', 'consultingServices']);
-	var myApp = angular.module('consulting',
-			['ngRoute', 'ngAnimate', 'ui.bootstrap', 'toaster', 'consultingControllers', 'consultingServices']);
+	var myApp = angular.module('consulting', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'toaster',
+								'consultingControllers', 'consultingServices']);
+
 	myApp.config([
 		'$routeProvider', '$locationProvider',
 		function ($routeProvider, $locationProvider) {
 			$routeProvider
-					.when('/members', {
-				templateUrl: 'members',
-				controller: 'MembersController',
-				controllerAs: 'vm'
-			})
-					.when('/competencyLevels', {
-				templateUrl: 'competencyLevels',
-				controller: 'CompetencyLevelsController',
-				controllerAs: 'vm'
-			})
-					.when('/objectiveAdmin', {
-				templateUrl: 'objectiveAdmin',
-				controller: 'ObjectiveAdminController',
-				controllerAs: 'vm'
-			})
-            		.when('/rankings', {
-				templateUrl: 'rankings',
-				controller: 'RankingsController',
-				controllerAs: 'vm'
-			})
-					.when('/:oid?', {
-				templateUrl: 'competencyProfile',
-				controller: 'CompetencyProfileController',
-				controllerAs: 'vm'
-			})
-					.when('/profileReport/:username', {
-				templateUrl: 'profileReport',
-				controller: 'ProfileReportController',
-				controllerAs: 'vm'
-			})
-					.otherwise({
-				redirectTo: '/'
-			});
-//			$locationProvider.html5Mode(true);
+				.when('/members', {
+					templateUrl: 'members',
+					controller: 'MembersController',
+					controllerAs: 'vm'
+				})
+				.when('/competencyLevels', {
+					templateUrl: 'competencyLevels',
+					controller: 'CompetencyLevelsController',
+					controllerAs: 'vm'
+				})
+				.when('/objectiveAdmin', {
+					templateUrl: 'objectiveAdmin',
+					controller: 'ObjectiveAdminController',
+					controllerAs: 'vm'
+				})
+				.when('/rankings', {
+					templateUrl: 'rankings',
+					controller: 'RankingsController',
+					controllerAs: 'vm'
+				})
+				.when('/:oid?', {
+					templateUrl: 'competencyProfile',
+					controller: 'CompetencyProfileController',
+					controllerAs: 'vm'
+				})
+				.when('/profileReport/:username', {
+					templateUrl: 'profileReport',
+					controller: 'ProfileReportController',
+					controllerAs: 'vm'
+				})
+				.otherwise({
+					redirectTo: '/'
+				});
+			//			$locationProvider.html5Mode(true);
 		}
-	]
-	);
-myApp.factory('authHttpResponseInterceptor',['$q','$window',function($q,$window){
+	]);
+
+	myApp.factory('authHttpResponseInterceptor', ['$q', '$window', function ($q, $window) {
 		return {
-				response: function(response){
-						if (response.status === 401) {
-								console.log('Response 401');
-						}
-						return response || $q.when(response);
-				},
-				responseError: function(rejection) {
-						if (rejection.status === 401) {
-								console.log('Response Error 401',rejection);
-//              $location.url('/login').search('returnTo', $location.path());
-								$window.location.href = '/login';
-								return;
-						}
-						return $q.reject(rejection);
+			response: function (response) {
+				if (response.status === 401) {
+					console.log('Response 401');
 				}
+				return response || $q.when(response);
+			},
+			responseError: function (rejection) {
+				if (rejection.status === 401) {
+					console.log('Response Error 401', rejection);
+					//              $location.url('/login').search('returnTo', $location.path());
+					$window.location.href = '/login';
+					return;
+				}
+				return $q.reject(rejection);
+			}
 		};
-}]);
-myApp.config(['$httpProvider',function($httpProvider) {
+	}]);
+
+	myApp.config(['$httpProvider', function ($httpProvider) {
 		//Http Intercpetor to check auth failures for xhr requests
 		$httpProvider.interceptors.push('authHttpResponseInterceptor');
 		$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-}]);
-		
+	}]);
+
 	myApp.run(['$location', '$route', '$rootScope', function ($location, $route, $rootScope) {
-			$location.skipReload = function () {
-				var lastRoute = $route.current;
-				var un = $rootScope.$on('$locationChangeSuccess', function () {
-					$route.current = lastRoute;
-					un();
-				});
-				return $location;
-			};
+		$location.skipReload = function () {
+			var lastRoute = $route.current;
+			var un = $rootScope.$on('$locationChangeSuccess', function () {
+				$route.current = lastRoute;
+				un();
+			});
 			return $location;
-		}]);
-}
-)(window.angular);
+		};
+		return $location;
+	}]);
+
+})(window.angular);
