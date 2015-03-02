0a98c7dbb945f484fc178288a6e45b03b7fd5571
ahhchuu <jkhiebert@gmail.com>
2015-03-01 22:00:32 -0600

    Converted to using mongoose instead of monk. Updates and finds both work.

--- competency-profile/api/competencyLevels.js
+++ competency-profile/api/competencyLevels.js
@@ -1,32 +1,28 @@
 ﻿/* REST API */
 
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
 var isAuthenticated = require('../config/auth');
 
+var CompetencyLevels = mongoose.model('CompetencyLevels');
+
 router.get('/', isAuthenticated, function (req, res) {
 
-	var collection = req.db.get('competencylevel');
-
-	collection.find({}, function (err, doc) {
+	CompetencyLevels.find({}, function (err, doc) {
 		if (err) {
 			res.send(err);
 		}
-		res.send({'data': doc});
+		res.send({ 'data': doc });
 	});
 });
 
 router.get('/:id', isAuthenticated, function (req, res) {
 	
-	var collection = req.db.get('competencylevel');
-	collection.findOne(
-		{
-			'_id' : req.params.id
-		}, 
-		function (err, doc) {
+	CompetencyLevels.findOne(req.params.id, function (err, doc) {
 		if (err) {
 			res.send(err);
-			}
+		}
 		res.send({ 'data': doc });
 	});
 });
@@ -34,24 +30,14 @@ router.get('/:id', isAuthenticated, function (req, res) {
 //TODO: consider change save to root to more closly align with REST API best practices
 //TODO: additionally, this route should make it clear that it's for a SINGLE competencylevel
 router.put('/:id', isAuthenticated, function (req, res) {
-	var db = req.db;
+
 	var level = req.body.level;
-	var collection = db.get('competencylevel');
-	collection.findAndModify(
-		{
-			query: { '_id': req.params.id },
-			update: level
-		},
-		{
-			'upsert': true
-		},
-		function (err, docs) {
-			if (err) {
-				res.send(err);
-			}
-			res.send({ 'result': 'success', 'level': docs });
-		});
-}
-);
+	CompetencyLevels.update(req.params.id, level, { upsert: true }, function (err, doc) {
+		if (err) {
+			res.send(err);
+		}
+		res.send({ 'result': 'success', 'level': doc });
+	});
+});
 
 module.exports = router;
