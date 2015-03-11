086b5140cf73b5f2271dc93d7d4cd5b941a93848
Chelsea Odell <codell@obsglobal.com>
2015-03-11 02:00:21 -0500

    design for profile report

--- competency-profile/public/app/controllers/profileReportController.js
+++ competency-profile/public/app/controllers/profileReportController.js
@@ -1,51 +1,67 @@
 (function () {
 	'use strict';
-
+	
 	angular
 		.module('consultingControllers')
 		.controller('ProfileReportController', ProfileReportController);
-
-	ProfileReportController.$inject = ['$routeParams', 'competencyProfileService', 'competencyLevelsService', 'toaster'];
-
-	function ProfileReportController($routeParams, competencyProfileService, competencyLevelsService, toaster) {
+	
+	ProfileReportController.$inject = ['$filter', '$routeParams', 'competencyProfileService', 'competencyLevelsService', 'toaster'];
+	
+	function ProfileReportController($filter, $routeParams, competencyProfileService, competencyLevelsService, toaster) {
 		var vm = this;
-
+		
 		vm.profile = {};
 		vm.username = $routeParams.username;
 		vm.showExamples = false;
 		vm.gateFilter = {};
 		vm.scoreFilter = {};
-
+		vm.showExamples = false;
+		vm.showRequiredExamples = false;
+		vm.toggleExamples = toggleExamples;
+		vm.toggleRequiredExamples = toggleRequiredExamples;
+		
 		initialize();
-
-		vm.toggleExamples = function toggleExamples() {
-			vm.showExamples = !vm.showExamples;
-		};
-
+		
 		// go get this user and the user document
 		function initialize() {
-
+			
 			var levels = competencyLevelsService.getCompetencyLevelLookups();
-
+			
 			competencyProfileService.getObjectivesByUsername(vm.username)
 				.then(
-					function (response) {
-						vm.profile = response;
-
-						vm.gateFilter = {
-							isMet: false,
-							gateLevel: response.summary.nextLevel
-						};
-
-						vm.scoreFilter = {
-							isMet: false,
-							gateLevel: "!" + response.summary.nextLevel
-						};
-					},
+				function (response) {
+					vm.profile = response;
+					
+					vm.gateFilter = {
+						isMet: false,
+						gateLevel: response.summary.nextLevel
+					};
+					
+					vm.scoreFilter = {
+						isMet: false,
+						gateLevel: "!" + response.summary.nextLevel
+					};
+				},
 					function (err) {
-						toaster.pop('error', 'Error Retrieving Objectives', err);
-					}
-				);
+					toaster.pop('error', 'Error Retrieving Objectives', err);
+				}
+			);
+		}
+		
+		function toggleExamples() {
+			var objectives = $filter('filter')(vm.profile.data, vm.scoreFilter);
+			
+			angular.forEach(objectives, function (objective) {
+				objective.open = vm.showExamples;
+			});
+		}
+		
+		function toggleRequiredExamples() {
+			var objectives = $filter('filter')(vm.profile.data, vm.gateFilter);
+
+			angular.forEach(objectives, function (objective) {
+				objective.open = vm.showRequiredExamples;
+			});
 		}
 	}
 })();
