94a30a6de2bd9347d32242b197829c5fdfbc9ea6
Quinn Wilson <git@midword.org>
2015-03-20 11:03:41 -0500

    isAdmin added to user on deserialize

--- competency-profile/config/passport.js
+++ competency-profile/config/passport.js
@@ -4,6 +4,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var extend = require('node.extend');
 var ActiveDirectory = require('activedirectory');
 var config = require('./passport.conf.js');
+var Q = require('q');
 
 var authConfig = {
 	url: 'ldap://ldap.obsglobal.com',
@@ -36,52 +37,78 @@ module.exports = function (passport) {
 				err = 'User: ' + username + ' not found.';
 			} else {
 				user.username = user.sAMAccountName;
-				if (!user.directReports) {
-					user.directReports = [];
+				var groupName = 'CPAdmin';
+				Q.all([
+					addIsAdmin(groupName, user, qad),
+					addDirectReports(user, qad)
+				]).then(function (results) {
 					return done(null, user);
-				} else {
-					var query = '(&!(userAccountControl:1.2.840.113556.1.4.803:=2)(manager=' + user.dn.replace(/\\/g, '\\\\').replace(/\*/, '\\*') + '))';
-					dr = {
-						filter: query,
-						scope: 'sub',
-						attributes: ['sAMAccountName'],
-					};
+				});
 
-					qad.findUsers(
-						dr,
-						function (err2, users) {
-							if (err2) {
-								return done(err2, null);
-							}
-							var reports = users.map(function (el) {
-								return el.sAMAccountName;
-							});
-							user.directReports = reports;
-							return done(null, user);
-						});
-				}
 			}
 		});
 	});
-	/*
-		passport.serializeUser(function (user, done) {
-			var sessionUser = {
-				sAMAccountName: user.sAMAccountName,
-				directreports: user.directreports,
-				userPrincipalName: user.userPrincipalName,
-				directreports: user.directreports,
-				displayName: user.displayName,
-				directReportsUsers: user.directReportsUsers
-			};
-			done(null, sessionUser)
-		})
 
-		passport.deserializeUser(function (sessionUser, done) {
-			// The sessionUser object is different from the AD
-			// it's actually req.session.passport.user and comes from the session collection
-			done(null, sessionUser)
-		})
-	*/
+	function addDirectReports(user, qad) {
+		var deferred = Q.defer();
+		if (!user.directReports) {
+			user.directReports = [];
+			deferred.resolve();
+		} else {
+			var query = '(&!(userAccountControl:1.2.840.113556.1.4.803:=2)(manager=' + user.dn.replace(/\\/g, '\\\\').replace(/\*/, '\\*') + '))';
+			dr = {
+				filter: query,
+				scope: 'sub',
+				attributes: ['sAMAccountName'],
+			};
+
+			qad.findUsers(
+				dr,
+				function (err2, users) {
+					if (err2) {
+						return done(err2, null);
+					}
+					var reports = users.map(function (el) {
+						return el.sAMAccountName;
+					});
+					user.directReports = reports;
+					deferred.resolve();
+				});
+		}
+		return deferred.promise;
+	}
+
+	function addIsAdmin(groupName, user, qad) {
+			var deferred = Q.defer();
+			var username = user.dn.replace(/\\/g, '\\\\').replace(/\*/, "\\*");
+			qad.isUserMemberOf(username, groupName, function (err, isMember) {
+				if (err) {
+					return done(err, false);
+				}
+				user.isAdmin = isMember;
+				deferred.resolve();
+			});
+			return deferred.promise;
+		}
+		/*
+			passport.serializeUser(function (user, done) {
+				var sessionUser = {
+					sAMAccountName: user.sAMAccountName,
+					directreports: user.directreports,
+					userPrincipalName: user.userPrincipalName,
+					directreports: user.directreports,
+					displayName: user.displayName,
+					directReportsUsers: user.directReportsUsers
+				};
+				done(null, sessionUser)
+			})
+
+			passport.deserializeUser(function (sessionUser, done) {
+				// The sessionUser object is different from the AD
+				// it's actually req.session.passport.user and comes from the session collection
+				done(null, sessionUser)
+			})
+		*/
 	passport.use('obslocal', new LocalStrategy(function (username, password, done) {
 		ad.authenticate(username, password, function (err, isAuthenticated) {
 			if (err) {
@@ -90,12 +117,21 @@ module.exports = function (passport) {
 			if (isAuthenticated) {
 				// strip off domain or @obsglobal.com
 				var sAMAccountName = username.replace(/^obs\\/i, '');
-				//				sAMAccountName = 'alevine';
+				//		sAMAccountName = 'bkatchnoski';
 				qad.findUser(sAMAccountName, function (err, user) {
 					if (!user) {
 						err = 'User: ' + username + ' not found.';
 						return done(err, false);
 					} else {
+						var groupName = 'CPAdmin';
+						qad.isUserMemberOf(sAMAccountName, groupName, function (err, isMember) {
+							if (err) {
+								return done(err, false);
+							}
+							user.isAdmin = isMember;
+						});
+
+
 						user.username = user.sAMAccountName;
 						if (!user.directReports) {
 							user.directReports = [];
