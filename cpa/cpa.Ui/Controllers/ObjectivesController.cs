74b529a1e459eab6ccbe7ff912f627739d498aad
caffgeek <gibble@gmail.com>
2015-06-17 15:04:56 -0500

    basic objectives admin routes

---
+++ cpa/cpa.Ui/Controllers/ObjectivesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using cpa.Shared;
+
+namespace cpa.Ui
+{
+	[Authorize]
+	public class ObjectivesController : ApiController
+	{
+		private readonly IProfileService _profileService;
+
+		public ObjectivesController(IProfileService profileService)
+		{
+			_profileService = profileService;
+		}
+
+		[HttpGet]
+		public List<ObjectiveModel> Get()
+		{
+			return new List<ObjectiveModel>();
+		}
+	}
+}
