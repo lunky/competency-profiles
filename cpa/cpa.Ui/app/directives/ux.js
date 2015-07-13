b66a2127eb9874dabf4c7aca770b89485b0df646
Neps <n3ps@users.noreply.github.com>
2015-07-13 09:28:15 -0500

    Focus on textarea when rendered; added directive

---
+++ cpa/cpa.Ui/app/directives/ux.js
@@ -0,0 +1,23 @@
+﻿//ui
+'use strict';
+
+(function () {
+    angular
+        .module('CompetencyProfilesDirectives')
+		.directive('obsFocus', focusOnRender);
+
+    focusOnRender.$inject = ['$timeout'];
+
+    function focusOnRender($timeout) {
+        var directive = {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                $timeout(function () {
+                    element[0].focus();
+                });
+            }
+        };
+
+        return directive;
+    }
+})(window.angular);
