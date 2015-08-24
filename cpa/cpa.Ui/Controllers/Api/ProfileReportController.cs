33e98825094103686661561ea9ba2167c866ec95
Neps <n3ps@users.noreply.github.com>
2015-08-24 15:10:20 -0500

    Added api level security to viewing profiles

--- cpa/cpa.Ui/Controllers/Api/ProfileReportController.cs
+++ cpa/cpa.Ui/Controllers/Api/ProfileReportController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Linq;
+using System.Web.Http;
 using AutoMapper;
 using cpa.Shared;
 
@@ -20,8 +22,9 @@ namespace cpa.Ui
         [HttpGet]
         public CompetencyProfileModel Get(string id)
         {
-            var displayName = _activeDirectoryUserService.GetTeamMemberByUsername(id).DisplayName;
-            
+            if (!IsAuthorized(id)) throw new UnauthorizedAccessException();
+
+            var displayName = _activeDirectoryUserService.GetTeamMemberByUsername(id).DisplayName;            
             var profileDto = _profileService.GetProfile(id);
             var profile = Mapper.Map<ProfileModel>(profileDto);
                         
@@ -31,6 +34,36 @@ namespace cpa.Ui
                 DisplayName = displayName,
                 UserId = profile.UserId
             };
+        }
+
+        private bool IsAuthorized(string user)
+        {
+            // Restrict to:
+            // a) Self
+            // b) Team members, if CM
+            // c) Anyone, if admin
+
+            var searchParams = new[]
+            {
+                "directreports",
+                "memberOf"
+            };
+
+            var self = User.Identity.Name;
+
+            var properties = _activeDirectoryUserService.GetProperties(self, searchParams);
+            var isCareerMentor = properties[searchParams[0]].Count > 0;
+            var isCpAdmin = properties[searchParams[1]].Cast<string>().Any(x => x.Contains("CPAdmin"));
+
+            if (isCpAdmin) return true;
+
+            if (isCareerMentor)
+            {
+                var team = _activeDirectoryUserService.GetTeamMembers(self);
+                return team.Any(t => t.UserId == user);
+            }
+            
+            return false;
         }
     }
 }
